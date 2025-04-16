38c3287fc274643765d1e34468c41ba9b25f873e
Azir <2581866471@qq.com>
2025-04-16 23:01:19 +0800

    feat:添加GraalVM构建

--- src/main/java/io/github/azirzsk/filedownloadhub/FileDownloadHubApplication.java
+++ src/main/java/io/github/azirzsk/filedownloadhub/FileDownloadHubApplication.java
@@ -1,14 +1,20 @@
 package io.github.azirzsk.filedownloadhub;
 
+import io.github.azirzsk.filedownloadhub.properties.FileProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
+@Slf4j
 @SpringBootApplication
 public class FileDownloadHubApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FileDownloadHubApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FileDownloadHubApplication.class, args);
+        FileProperties fileProperties = applicationContext.getBean(FileProperties.class);
+        log.info("基础路径为：{}", fileProperties.getBasePath());
     }
 
 }
