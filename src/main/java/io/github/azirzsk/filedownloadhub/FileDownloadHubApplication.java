de4c3937d8bb8a180bc93111d55bbf476fe6f82e
Azir <2581866471@qq.com>
2025-04-20 15:27:53 +0800

    bugfix:修复端口获取问题

--- src/main/java/io/github/azirzsk/filedownloadhub/FileDownloadHubApplication.java
+++ src/main/java/io/github/azirzsk/filedownloadhub/FileDownloadHubApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StringUtils;
 
 
 @Slf4j
@@ -15,7 +16,8 @@ public class FileDownloadHubApplication {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(FileDownloadHubApplication.class, args);
         FileProperties fileProperties = applicationContext.getBean(FileProperties.class);
         log.info("基础路径为：{}", fileProperties.getBasePath());
-        log.info("文件下载服务启动成功，地址：{}", "http://localhost:%s/".formatted(applicationContext.getEnvironment().getProperty("server.port")));
+        String port = applicationContext.getEnvironment().getProperty("server.port");
+        log.info("文件下载服务启动成功，地址：{}", "http://localhost:%s/".formatted(StringUtils.hasText(port) ? port : "8080"));
     }
 
 }
